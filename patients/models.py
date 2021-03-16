b4aafd347dd58a83ca9571d1e78c164592318584
Alejandro Calzadilla <alejandrocalzadilla@gmail.com>
2021-03-16 12:02:03 -0400

    added patient migrations and superuser

--- patients/models.py
+++ patients/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Patient(models.Model):
+  name = models.CharField(max_length=100)
+  first_name = models.CharField(max_length=100)
+  sex_at_birth = models.CharField(max_length=50)
+  email = models.EmailField(max_length=254)
+  notes = models.TextField()
